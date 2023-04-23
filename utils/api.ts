0466a682ee2b2bb600a9b7f60706de2f08b840ea
junhea <97426534+junhea@users.noreply.github.com>
2023-04-23 20:40:02 +0900

    ✨ feat: api call

--- utils/api.ts
+++ utils/api.ts
@@ -17,3 +17,26 @@ export function formatDate(date: Date) {
     date.getDate() === 0 ? '' : `.${date.getDate()}`
   }`;
 }
+
+export async function getPostsByDate(date: Date) {
+  // return (
+  //   await axiosInstance.get(`/posts/${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`)
+  // ).data;
+
+  return new Array(30).fill({
+    title: 'asdf',
+    type: 'mentoring',
+    currentCount: 1,
+    totalCount: 2,
+    created: date,
+    scheduled: new Date(2023, 3, 26),
+    author: 'asdf',
+  });
+}
+
+export function dday(date: Date) {
+  const today = new Date();
+  today.setHours(0);
+  const timeDiff = today.getTime() - date.getTime();
+  return `D${Math.ceil(timeDiff / (1000 * 3600 * 24)) - 1}`;
+}
