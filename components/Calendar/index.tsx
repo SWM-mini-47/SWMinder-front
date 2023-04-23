52be32a3b03a705ce4616965eb1ce5105b0e53ec
junhea <97426534+junhea@users.noreply.github.com>
2023-04-23 21:51:44 +0900

    ✨ feat: save posts and date in global context

--- components/Calendar/index.tsx
+++ components/Calendar/index.tsx
@@ -1,11 +1,7 @@
 import { css } from '@emotion/react';
 import CalendarCell from '../CalendarCell';
-
-interface CalendarProps {
-  date: Date;
-  posts: Post[][];
-  cellClickCallback: (day: number) => void;
-}
+import { monthlyPosts, selectedDate } from '@/states/dateContext';
+import { useRecoilState, useRecoilValueLoadable } from 'recoil';
 
 const style = css`
   border: 0;
@@ -18,9 +14,11 @@ const style = css`
   }
 `;
 
-export default function Calendar({ date, posts, cellClickCallback }: CalendarProps) {
+export default function Calendar() {
+  const [date, setDate] = useRecoilState(selectedDate);
+  const posts = useRecoilValueLoadable(monthlyPosts);
   const startOffset = new Date(date.getFullYear(), date.getMonth() + 1, 1).getDay();
-  const daysInMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate() + 1;
+  const daysInMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
 
   return (
     <table css={style}>
@@ -42,10 +40,15 @@ export default function Calendar({ date, posts, cellClickCallback }: CalendarPro
                     <td
                       key={`calendarCell${d}`}
                       onClick={() => {
-                        cellClickCallback(d - 1);
+                        setDate(new Date(date.getFullYear(), date.getMonth(), d));
                       }}
                     >
-                      <CalendarCell day={d} posts={posts[d - 1]} enabled={true} />
+                      <CalendarCell
+                        day={d}
+                        highlight={d === date.getDate()}
+                        posts={posts.state === 'hasValue' ? posts.getValue()[d - 1] : []}
+                        enabled={true}
+                      />
                     </td>
                   );
               })}
--- states/dateContext.ts
+++ states/dateContext.ts
@@ -1,6 +1,14 @@
-import { atom } from 'recoil';
+import { getPostsByDate, getPostsByMonth } from '@/utils/api';
+import { atom, selector } from 'recoil';
 
 export const selectedDate = atom<Date>({
   key: 'selectedDate',
   default: new Date(),
+});
+
+export const monthlyPosts = selector({
+  key: 'posts',
+  get: async ({ get }) => {
+    return await getPostsByMonth(get(selectedDate));
+  },
 });
