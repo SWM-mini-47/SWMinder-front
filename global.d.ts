3de4310d2082e71a71d8fe501334ba709376cadb
junhea <97426534+junhea@users.noreply.github.com>
2023-04-23 20:39:46 +0900

    ✨ feat: add attributes to Post

--- global.d.ts
+++ global.d.ts
@@ -2,4 +2,9 @@ type PostType = 'mentoring' | 'meetup' | 'board';
 interface Post {
   title: string;
   type: PostType;
+  currentCount: number;
+  totalCount: number;
+  created: Date;
+  scheduled: Date;
+  author: string;
 }
