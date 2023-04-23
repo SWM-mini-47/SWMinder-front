c2fc3f7f9b013d145f7e7e30b9759d74f6ac7f83
junhea <97426534+junhea@users.noreply.github.com>
2023-04-23 18:07:01 +0900

    ✨ feat: postDetail component

---
+++ components/PostDetail/index.tsx
@@ -0,0 +1,118 @@
+import { formatDate } from '@/utils/api';
+import { css } from '@emotion/react';
+import Link from 'next/link';
+
+interface PostDetailProps {
+  id: string;
+  title: string;
+  author: string;
+  category: string;
+  created: Date;
+  scheduled: Date;
+  totalCount: number;
+  currentCount: number;
+  url: string;
+  closeCallback: () => void;
+}
+
+const style = {
+  dim: css`
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background-color: #0000000a;
+  `,
+  container: css`
+    position: relative;
+    padding: 30px 20px 30px 20px;
+    left: 50%;
+    transform: translateX(-50%);
+    min-width: 900px;
+    width: 70%;
+    background-color: white;
+    h3 {
+      height: 30px;
+      line-height: 30px;
+      font-size: 15px;
+      margin-left: 10px;
+      color: #494949;
+      display: inline-block;
+      width: 150px;
+    }
+    a {
+      float: right;
+      width: 180px;
+      text-align: center;
+      display: block;
+      line-height: 30px;
+      margin: 10px;
+      text-decoration: underline 1px black;
+      color: #2c1a7c;
+      font-size: 20px;
+    }
+  `,
+  breaker: css`
+    width: 100%;
+    height: 1px;
+    background-color: #aaaaaa;
+  `,
+  closeButton: css`
+    position: absolute;
+    font-size: 30px;
+    font-weight: bold;
+    border: none;
+    background-color: white;
+    padding: 20px;
+    right: 0;
+    top: 0;
+  `,
+};
+
+export default function PostDetail({
+  id,
+  title,
+  author,
+  category,
+  created,
+  scheduled,
+  totalCount,
+  currentCount,
+  closeCallback,
+  url,
+}: PostDetailProps) {
+  return (
+    <>
+      <div css={style.dim} />
+      <div css={style.container}>
+        <button css={style.closeButton} onClick={closeCallback}>
+          X
+        </button>
+        <h3>제목</h3>
+        <span>{title}</span>
+        <div css={style.breaker} />
+        <h3>분류</h3>
+        <span>{category}</span>
+        <div css={style.breaker} />
+        <h3>게시자</h3>
+        <span>{author}</span>
+        <div css={style.breaker} />
+        <h3>게시 일자</h3>
+        <span>{formatDate(created)}</span>
+        <div css={style.breaker} />
+        <h3>예정 일자</h3>
+        <span>{formatDate(scheduled)}</span>
+        <div css={style.breaker} />
+        <h3>모집 현황</h3>
+        <span>{`${currentCount}/${totalCount}`}</span>
+        <div css={style.breaker} />
+        <Link href={url}>접수하러 가기</Link>
+        <div>
+          {/* todo: comments */}
+          <h3>댓글</h3>
+        </div>
+      </div>
+    </>
+  );
+}
