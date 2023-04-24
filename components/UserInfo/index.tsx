0f60708f12dc659b301fb713a06a003aa97a5de9
junhea <97426534+junhea@users.noreply.github.com>
2023-04-24 11:49:43 +0900

    ✨ feat: user context 정의 및 userInfo 구현

---
+++ components/UserInfo/index.tsx
@@ -0,0 +1,42 @@
+import { currentUser } from '@/states/userContext';
+import { css } from '@emotion/react';
+import { useRecoilValue } from 'recoil';
+import DefaultPic from '@/assets/person.svg';
+import Image from 'next/image';
+
+interface UserInfoProps {
+  onClick: () => void;
+}
+
+const style = css`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  svg,
+  img {
+    border-radius: 50%;
+    width: 100px;
+    height: 100px;
+  }
+  p {
+    margin-left: 15px;
+    font-size: 25px;
+    font-weight: bold;
+  }
+`;
+
+export default function UserInfo({ onClick }: UserInfoProps) {
+  const user = useRecoilValue(currentUser);
+  return (
+    <div css={style} onClick={onClick}>
+      {user.profileImage ? (
+        <Image width={100} height={100} src={user.profileImage} alt={'profile picture'} />
+      ) : (
+        <DefaultPic />
+      )}
+      <p>{user.username}</p>
+    </div>
+  );
+}
