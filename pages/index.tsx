b1f9e016ec7ca938c21b93ce022317c14ff18d79
yunyongjae <yyj960523@gmail.com>
2023-04-26 16:21:05 +0900

    💄 #16

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,16 @@
+import Profile from '@/components/Profile';
+
 export default function Home() {
-  return <></>;
+  return (
+    <Profile
+      name={'윤용재'}
+      phone={'010-9091-9349'}
+      school={'소마대'}
+      birth={new Date()}
+      email={'asdf'}
+      address={'asdf'}
+      skills={['java', 'android', 'python', 'spring']}
+      description={'안녕하세요 저는 이번에 ...... 잘부탁드립니다'}
+    />
+  );
 }
