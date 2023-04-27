bc76262ce6dc284cf554f6205f4bee46a2e248bf
junhea <97426534+junhea@users.noreply.github.com>
2023-04-28 04:32:33 +0900

    🚑 hotfix: basePath

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const withPWA = require('next-pwa')({
 
 module.exports = withPWA({
   reactStrictMode: true,
-  basePath: process.env.NODE_ENV === 'production' ? '/' : '',
+  basePath: '',
   images: {
     unoptimized: process.env.NODE_ENV === 'production',
   },
